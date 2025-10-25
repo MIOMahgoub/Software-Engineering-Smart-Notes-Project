8db1940c5215a875e981f4dba057673eff076b08
Muhammad <mmahgoub2012@fau.edu>
2025-10-25 18:18:55 -0400

    Set up uploads app and working /upload/ route

---
+++ uploads/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def upload_file(request):
+    return HttpResponse("Upload page placeholder — feature coming soon!")
+
+# Create your views here.
+
